708665c18da5ef01245d385fa75a284ab8140c13
Limonrana <limonrana515@gmail.com>
2022-06-19 09:10:52 +0600

    Setup Layout

--- src/plugins/bootstrap-vue.js
+++ src/plugins/bootstrap-vue.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 
-import BootstrapVue from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,51 @@
 <template>
     <div class="wrapper">
-        <div class="main-content"></div>
+        <!-- Header title section start here -->
+        <div class="header pb-3">
+            <div class="container-fluid">
+                <h2 class="h2 mb-0">Members</h2>
+            </div>
+        </div>
+        <!-- Header title section end here -->
+        <div class="container-fluid">
+            <!-- Top bar section start here -->
+
+            <!-- TODO: Top bar section start here -->
+
+            <!-- Top bar section end here -->
+            <!-- Filter bar section start here -->
+
+            <!-- TODO: Filter bar section start here -->
+
+            <!-- Filter bar section end here -->
+            <!-- Member list section start here -->
+            <div class="main-content">
+                <div class="tab-content">
+                    <div class="tab-pane fade show active">
+                        <!-- TODO: Member list section start here -->
+                    </div>
+                </div>
+            </div>
+            <!-- Member list section end here -->
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     name: 'Home',
+    data() {
+        return {
+            isLoading: true,
+            search: '',
+        }
+    },
+    mounted() {
+        setTimeout(() => {
+            this.isLoading = false
+        }, 1000)
+    },
 }
 </script>
+
+<style scoped lang="scss"></style>
